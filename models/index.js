ce789d95815266b75709c0bca94d3c9aff61b24f
Frederick Articuno <mckiyoshi@gmail.com>
2019-11-12 20:35:20 -0500

    trying to add jawsdb attempt 3

--- models/index.js
+++ models/index.js
@@ -22,8 +22,8 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: 'hacktheplanet',
-		database: 'todoagain_db'
+		password: 'password',
+		database: 'burgers_db'
 	});
 };
 //===============================================================
